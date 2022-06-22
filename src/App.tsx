791a7e7709dee9a61155b6d613f78db3bed9ac02
Samuel Gallo <samuel@samuelgallo.com>
2022-06-22 00:12:47 -0400

    include components and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      slug
-      teacher {
-        name
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-  slug: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  // useEffect(() => {
-  //   client.query({ query: GET_LESSONS_QUERY }).then((response) => {
-  //     console.log(response.data);
-  //   });
-  // }, []);
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson, index) => {
-        return <li key={index}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
